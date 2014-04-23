cc379382949a8d36866b824b3e92da4f0ee846bb
Sean Leather <sean.leather@gmail.com>
2014-04-23 09:08:13 -0700

    Add HubSpot module

---
+++ src/Web/HubSpot.hs
@@ -0,0 +1,9 @@
+module Web.HubSpot
+  ( module Web.HubSpot.Types
+  , module Web.HubSpot.Auth
+  ) where
+
+--------------------------------------------------------------------------------
+
+import Web.HubSpot.Types
+import Web.HubSpot.Auth
