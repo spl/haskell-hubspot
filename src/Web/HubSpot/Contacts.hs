f92aabde0563ece5255cc78bcc7894be4b743d1b
Sean Leather <sean.leather@gmail.com>
2014-04-28 12:34:46 -0700

    Renames

--- src/Web/HubSpot/Contacts.hs
+++ src/Web/HubSpot/Contacts.hs
@@ -52,7 +52,7 @@ getContact
   -> Manager
   -> m Contact
 getContact key = generalRequest
-  ["https://api.hubapi.com/contacts/v1/contact", contactKey key, contactKeyVal key, "profile"]
+  ["https://api.hubapi.com/contacts/v1/contact", keyName key, keyVal key, "profile"]
   return
   (jsonContent "getContact")
 
@@ -66,9 +66,9 @@ getContacts
   -> Manager
   -> m (HashMap ContactId Contact)
 getContacts []   = \_ _ -> return HM.empty
-getContacts keys = let key = contactKey (head keys) in generalRequest
+getContacts keys = let key = keyName (head keys) in generalRequest
   ["https://api.hubapi.com/contacts/v1/contact", key `TS.snoc` 's', "batch"]
-  (addQuery $ queryTextToQuery $ map ((key,) . Just . contactKeyVal) keys)
+  (addQuery $ queryTextToQuery $ map ((key,) . Just . keyVal) keys)
   (liftM (HM.fromList . map (first read) . HM.toList) . jsonContent "getContacts")
 
 -- | Update a contact profile by a 'ContactId'
@@ -80,27 +80,27 @@ updateContact
   -> Manager
   -> m ()
 updateContact contactId propValues = generalRequest
-  ["https://api.hubapi.com/contacts/v1/contact/vid", contactKeyVal contactId, "profile"]
+  ["https://api.hubapi.com/contacts/v1/contact/vid", keyVal contactId, "profile"]
   (setJSONBody $ PropValueList propValues)
   (\_ -> return ())
 
 --------------------------------------------------------------------------------
 
 class ContactKey key where
-  contactKey    :: key -> Text
-  contactKeyVal :: key -> Text
+  keyName :: key -> Text
+  keyVal  :: key -> Text
 
 -- | https://developers.hubspot.com/docs/methods/contacts/get_contact
 instance ContactKey ContactId where
-  contactKey _ = "vid"
-  contactKeyVal = TS.pack . show
+  keyName _ = "vid"
+  keyVal = TS.pack . show
 
 -- | https://developers.hubspot.com/docs/methods/contacts/get_contact_by_utk
 instance ContactKey UserToken where
-  contactKey _ = "utk"
-  contactKeyVal = TS.decodeUtf8 . fromUserToken
+  keyName _ = "utk"
+  keyVal = TS.decodeUtf8 . fromUserToken
 
 -- | https://developers.hubspot.com/docs/methods/contacts/get_contact_by_email
 instance ContactKey Text where
-  contactKey _ = "email"
-  contactKeyVal = id
+  keyName _ = "email"
+  keyVal = id
